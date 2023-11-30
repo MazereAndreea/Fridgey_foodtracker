fb435d8f99ff4a5a610c0a2347706a019b4b2e27
Andreea <76445060+MazereAndreea@users.noreply.github.com>
2023-11-30 20:01:56 +0200

    layout for the next page, when the user chooses an item from the list, the next page prompts them to say where they put it.

---
+++ app/src/main/java/com/example/fridgey/location.kt
@@ -0,0 +1,11 @@
+package com.example.fridgey
+
+import android.app.Activity
+import android.os.Bundle
+
+class location : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.location)
+    }
+}
