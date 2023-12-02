cbbe50c7690aae45b81ddaee77202b0f7cc45a4a
Andreea <76445060+MazereAndreea@users.noreply.github.com>
2023-12-02 20:59:11 +0200

    All the data classes that represent information about the food (from EDAMAM API)

---
+++ app/src/main/java/com/example/fridgey/Nutrients.kt
@@ -0,0 +1,9 @@
+package com.example.fridgey
+
+data class Nutrients(
+    val CHOCDF: Double,
+    val ENERC_KCAL: Int,
+    val FAT: Double,
+    val FIBTG: Int,
+    val PROCNT: Double
+)
---
+++ app/src/main/java/com/example/fridgey/NutrientsX.kt
@@ -0,0 +1,9 @@
+package com.example.fridgey
+
+data class NutrientsX(
+    val CHOCDF: Int,
+    val ENERC_KCAL: Int,
+    val FAT: Double,
+    val FIBTG: Int,
+    val PROCNT: Double
+)
