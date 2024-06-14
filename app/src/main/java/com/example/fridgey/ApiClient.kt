befe3e3f9bf057fffc80b09a205ddb295e52fc08
Andreea <76445060+MazereAndreea@users.noreply.github.com>
2024-06-14 16:34:06 +0300

    the URL was wrong

--- app/src/main/java/com/example/fridgey/ApiClient.kt
+++ app/src/main/java/com/example/fridgey/ApiClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+    private const val BASE_URL = "api/food-database/v2/parser"
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
