51f523e5105fe03bf6d90a4ce0574f09ab28b0b6
Andreea <76445060+MazereAndreea@users.noreply.github.com>
2024-06-21 19:49:37 +0300

    dont need it for now, i will add more functionality to this page later

--- app/src/main/java/com/example/fridgey/SearchBarActivity.kt
+++ app/src/main/java/com/example/fridgey/SearchBarActivity.kt
@@ -3,14 +3,15 @@ package com.example.fridgey
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.ExperimentalMaterial3Api
 import composables.SimpleDockedSearchBar
 
-class SearchBarActivity : ComponentActivity() {
+class SearchBarActivity(function: () -> Unit) : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            val foodList = getFoodList()
-//            SimpleDockedSearchBar(foodList)
+
         }
     }
 }
