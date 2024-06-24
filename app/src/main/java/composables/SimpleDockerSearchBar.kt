7cfb90d745a6ed6199699c0fb5fb1ee3096078b1
Andreea <76445060+MazereAndreea@users.noreply.github.com>
2024-06-24 13:07:13 +0300

    updating code because i changed navigation structure

--- app/src/main/java/composables/SimpleDockerSearchBar.kt
+++ app/src/main/java/composables/SimpleDockerSearchBar.kt
@@ -1,41 +1,56 @@
 package composables
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SimpleDockedSearchBar(navigateToMainScreen : () -> Unit, /*foodList: MutableList<String>*/) {
+fun SimpleDockedSearchBar(navController: NavHostController) {
     var text by remember { mutableStateOf("") }
     var active by remember { mutableStateOf(false) }
     val listToSeeFunctionality = mutableListOf<String>("Cartof dulce", "ceapa", "masline", "magiun", "mamaliga")
 
     Scaffold(
+        modifier = Modifier,
         topBar = {
-            TopAppBar(
-                title = { Text(text = "Simple Scaffold Example") }
+            TextField(
+                value = "Enter something",
+                onValueChange = { text = it },
+                colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = androidx.compose.ui.graphics.Color.White,
+                    focusedContainerColor = androidx.compose.ui.graphics.Color.White
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp)
+                    .border(BorderStroke(2.dp, androidx.compose.ui.graphics.Color.Cyan), shape = CircleShape)
             )
         },
-
-        content = { paddingValues ->
+        bottomBar = {
+            Text(text = "Bottom Bar")
+        },
+        content = {
             Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues),
+                modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-
-                Button(onClick = navigateToMainScreen) {
-                    Text(text = "Welcome to SearchBarActivity")
+                horizontalAlignment = Alignment.CenterHorizontally,
+                content = {
+                    Text(text = "Search Bar")
+                    Button(onClick = { navController.navigate(route = NavigationRoutes.main_screen.route) }) {
+                        Text(text = "Back to Main Page")
+                    }
                 }
-            }
+            )
         }
     )
 }
-
