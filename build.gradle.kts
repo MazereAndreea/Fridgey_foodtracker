9f31a6df6c696dad9c4ed0ac929f25b36db68fc2
Adrian Cirstea <54354938+ShadowsAdi@users.noreply.github.com>
2024-08-26 01:13:39 +0300

    Refactor API secrets handling

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,16 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
+
+val outputFile = File("$projectDir", "api_secrets.properties")
+
+outputFile.apply {
+    parentFile.mkdirs()
+
+    writeText("# Get your API Key and APP ID from here:\n" +
+            "# https://developer.edamam.com/food-database-api\n" +
+            "APP_ID = \"your_appid\"\n" +
+            "APP_KEY = \"your_aptikey\"")
+
+    println("API Secrets file generated in: ${outputFile.absolutePath}")
+}
